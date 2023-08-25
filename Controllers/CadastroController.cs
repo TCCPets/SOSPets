c66f17b805e29ff1648015c446eed0a8f27bf697
Kessy <Kessy@DESKTOP-QF3CAIG>
2023-08-25 08:59:38 -0300

    .

---
+++ Controllers/CadastroController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SOSPets.Controllers
+{
+    public class CadastroController : Controller
+    {
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+    }
+}
